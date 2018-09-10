857c011b314de2a49ce5ecce00d30b3b11f3b50a
sam grimmer <samg@iqualify.com>
2018-09-10 21:49:01 +1200

    Fixed user controller swagger defs, by adding the required decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 
 import { UserDto } from '../dto/user.dto';
 import { UserService } from './user.service';
-import { ApiUseTags } from '@nestjs/swagger';
+import { ApiUseTags, ApiOkResponse, ApiCreatedResponse, ApiBadRequestResponse, ApiImplicitParam } from '@nestjs/swagger';
+import { ApiException } from 'shared/api.exception.module';
 
 @ApiUseTags('user')
 @Controller('user')
@@ -11,16 +12,23 @@ export class UserController {
     constructor(private readonly userService: UserService) { }
 
     @Get()
+    @ApiOkResponse({ type: UserDto, isArray: true })
+    @ApiBadRequestResponse({ type: ApiException })
     async getAll(): Promise<UserDto[]> {
         return this.userService.findAll();
     }
 
     @Get(':email')
+    @ApiOkResponse({ type: UserDto })
+    @ApiBadRequestResponse({ type: ApiException })
+    @ApiImplicitParam({ name: 'email', required: true })
     async Get(@Param('email') email): Promise<any> {
         return this.userService.findByEmail(email);
     }
 
     @Post()
+    @ApiCreatedResponse({ type: UserDto })
+    @ApiBadRequestResponse({ type: ApiException })
     async Create(@Body() userDto: UserDto): Promise<UserDto> {
         return await this.userService.create(userDto);
     }
